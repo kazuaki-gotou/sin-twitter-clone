f7f8e4d964783fddaf7af57cbe7f4f60ba732d2e
kazuaki-gotou <k.as.bruder@gmail.com>
2019-01-27 20:33:26 +0900

    タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Caniborrow
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.log_level = :info
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
 
     # Settings in config/environments/* take precedence over those specified here.
