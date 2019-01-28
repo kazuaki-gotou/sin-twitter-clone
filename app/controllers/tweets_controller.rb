31effaa2105293bfdd6542a89f2abc2d518830f0
kazuaki-gotou <k.as.bruder@gmail.com>
2019-01-28 17:00:33 +0900

    scaffoldでできたコメントの削除

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,18 +1,18 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :update, :destroy, :edit]
 
-  # GET /tweets
-  # GET /tweets.json
+  
+  
   def index
     @tweets = Tweet.all
   end
 
-  # GET /tweets/1
-  # GET /tweets/1.json
+  
+  
   def show
   end
 
-  # GET /tweets/new
+  
   def new
     if params[:back]
       @tweet = Tweet.new(tweet_params)
@@ -22,12 +22,12 @@ class TweetsController < ApplicationController
     
   end
 
-  # GET /tweets/1/edit
+  
   def edit
   end
 
-  # POST /tweets
-  # POST /tweets.json
+  
+  
   def create
     @tweet = Tweet.new(tweet_params)
 
@@ -42,8 +42,8 @@ class TweetsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tweets/1
-  # PATCH/PUT /tweets/1.json
+  
+  
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
@@ -56,8 +56,8 @@ class TweetsController < ApplicationController
     end
   end
 
-  # DELETE /tweets/1
-  # DELETE /tweets/1.json
+  
+  
   def destroy
     @tweet.destroy
     respond_to do |format|
@@ -72,12 +72,12 @@ class TweetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+   
     def set_tweet
       @tweet = Tweet.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+   
     def tweet_params
       params.require(:tweet).permit(:content)
     end
