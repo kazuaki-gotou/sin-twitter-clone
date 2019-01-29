e84888da40f61c5e28bb84de9eb5f6dcfd7ca804
kazuaki-gotou <k.as.bruder@gmail.com>
2019-01-29 13:47:34 +0900

    指摘内容の修正（主にコードの整理）

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,56 +8,40 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
-  
-  
   def show
   end
-
   
   def new
     if params[:back]
       @tweet = Tweet.new(tweet_params)
     else
       @tweet = Tweet.new
-    end
+    end    
   end
 
-  
   def edit
   end
-
-  
   
   def create
     @tweet = Tweet.new(tweet_params)
-    respond_to do |format|
       if @tweet.save
         format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
       else
         format.html { render :new }
       end
-    end
   end
 
-  
-  
   def update
-    respond_to do |format|
-      if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
+    if @tweet.update(tweet_params)
+      format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
+    else
+      format.html { render :edit }
     end
   end
 
-  
-  
   def destroy
     @tweet.destroy
-    respond_to do |format|
       format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
-    end
   end
 
   def confirm
@@ -66,13 +50,12 @@ class TweetsController < ApplicationController
   end
 
   private
-   
-    def set_tweet
-      @tweet = Tweet.find(params[:id])
-    end
 
-   
-    def tweet_params
-      params.require(:tweet).permit(:content)
-    end
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+      
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
 end
